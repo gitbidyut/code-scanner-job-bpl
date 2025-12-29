1fa67adb7734faa48bf5faeda43ca11edc735685
Bidyut Pal <bidyut.pal@atos.net>
2025-12-29 18:24:43 +0530

    access key add

--- scan_repo.py
+++ scan_repo.py
@@ -8,7 +8,7 @@ import boto3
 # Configuration
 # =========================
 ENDPOINT_NAME = "credential-scanner-endpoint"
-LAMBDA_NAME = "disable-access-key"
+LAMBDA_NAME = "lambda_function"
 REGION = "us-east-1"
 THRESHOLD = 0.80
 
