3252c1a2d337d47c1bbc12fdbca3582f2cbcf022
gitbidyut <web.pal.bidyut@gmail.com>
2025-12-30 11:07:18 +0530

    Update variables.tf

--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -1,11 +1,12 @@
 variable "lambda_name" {
   description = "Lambda function name"
   type        = string
-  default     = "disable-access-key-bpl"
+  default     = "lambda_function"
 }
 
 variable "region" {
   description = "AWS region"
   type        = string
   default     = "us-east-1"
+
 }
